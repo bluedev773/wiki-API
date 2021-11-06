8b90fda736a09813f33d1c9837dac24a5e19d808
Matthew Litvinov <matthew.litvinov@gmail.com>
2021-11-05 20:48:59 -0400

    PUT a single article

--- app.js
+++ app.js
@@ -68,6 +68,19 @@ app.route("/articles/:articleTitle")
                 res.send("No articles matching that title were found.");
             }
         });
+    })
+
+    .put(function(req, res){
+        Article.findOneAndUpdate(
+            {title: req.params.articleTitle},
+            {title: req.body.title, content: req.body.content},
+            {overwrite: true},
+            function(err){
+                if(!err){
+                    res.send("Successfully updated article.");
+                }
+            }
+        );
     });
 
 
